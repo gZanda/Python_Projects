5ef6ed8904d635005f051ec64aa3395cf8f9bafd
gZanda <gabrielfzanda@gmail.com>
2023-05-11 00:51:52 -0300

    Organization

--- 1 - Face Detector/SinglePhoto.py
+++ 1 - Face Detector/SinglePhoto.py
@@ -11,7 +11,7 @@ faces = detect.detectMultiScale(gray, 1.3, 5)   # x, y, Width and Height ( coord
 for(x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0), 2) # img, coordinates, wid and hei, color, thickness
 
-cv2.imshow("Face Tracker", img)
+cv2.imshow("Face Detection", img)
 cv2.waitKey(0)  # Image will be oppened untill closed
 impImg1.release()
 cv2.destroyAllWindows()
